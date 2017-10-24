08c53b0271adf18c2f037f3ddd48c6b3a342936b
KaichenZhang <ac1102919258@gmail.com>
2017-10-23 23:03:42 -0400

    server url

--- server.py
+++ server.py
@@ -51,4 +51,4 @@ api.add_resource(GetLog, '/get') # Route_1
 api.add_resource(HandleRequests, '/post') # Route_2
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port = 5005)
+    app.run(host='0.0.0.0', port=5005, threaded=True)
